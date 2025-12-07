bb9628e0a65ec1c373887990be672832b20dbce2
ajamol2197 <ajamol2197@gmail.com>
2025-12-07 14:58:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1
+a = input("chiroq rangni kirit: ")
+if a == "qizil":
+    print("toxtang")
+elif a == "sariq":
+    print("tayorlaning")
+elif a == "yashhil":
+    print("yuring")
+else:
+    print("bunday rang yoq")
+
+# 2
+a = int(input("internet tezligini kirit: "))
+if a >=100:
+    print("sizda ajayib tezlik")
+elif a >= 50:
+    print("tezlik yaxshi")
+elif a >= 10:
+    print("tezlik past")
+elif a <10:
+    print("bu tezlik bilan hech nima qilib bolmaydi")
+
+# 3
+a = int(input("ball kirit: "))
+if a >= 90:
+    print("a'lo")
+elif a >= 75:
+    print("yaxshi")
+elif a >= 50:
+    print("qoniqarli")
+elif a < 50:
+    print("yiqildingiz")
